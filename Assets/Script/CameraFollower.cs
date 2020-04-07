24e1ad2838d5bea25a3517dec356d28f0cd4e6ae
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2020-04-06 17:55:03 -0700

    Add script to generate the lane and also the lane segments.

---
+++ Assets/Script/CameraFollower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField]
+    Transform follow;
+
+    [SerializeField]
+    Vector3 cameraOffSet;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = follow.transform.position + cameraOffSet;
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<LaneController.Lane> lanes = FindObjectOfType<LaneController>().LevelSegments[0].lanes;
+        transform.position = lanes[lanes.Count / 2].laneSegments[0].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * speed; 
+    }
+}
