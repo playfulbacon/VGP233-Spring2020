fd2c30ab6aa38c3a9b5f8bfc946dffee0f601161
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2020-04-08 22:55:25 -0700

    Update week1
    - Create ScoreManager to count score when the the player touch in the 'coins'.
    - Update the UI using canvas and text component.

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -3,25 +3,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Properties")]
     [SerializeField]
-    float speed = 1f;
+    private float mSpeed = 15f;
+
+    [SerializeField]
+    private ScoreManager mScoreManager = null;
 
     private List<LaneController.Lane> lanes;
 
     private int mSwitchLanes = 0;
     void Start()
     {
-        //List<LaneController.Lane> lanes = FindObjectOfType<LaneController>().LevelSegments[0].lanes;
+        mScoreManager = FindObjectOfType<ScoreManager>();
+
         lanes = FindObjectOfType<LaneController>().LevelSegments[0].lanes;
         transform.position = lanes[lanes.Count >> 1].laneSegments[0].transform.position;
+
     }
 
     void Update()
     {
-        transform.position = new Vector3(lanes[mSwitchLanes].laneSegments[0].transform.position.x, transform.position.y, transform.position.z);
-        transform.position += transform.forward * Time.deltaTime * speed;
+        Movement();
+    }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+    private void Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
         {
             if (mSwitchLanes > 0)
             {
@@ -29,12 +37,25 @@ public class Player : MonoBehaviour
             }
 
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
             if (mSwitchLanes < lanes.Count - 1)
             {
                 mSwitchLanes++;
             }
+        }
+
+        transform.position = new Vector3(lanes[mSwitchLanes].laneSegments[0].transform.position.x, transform.position.y, transform.position.z);
+        transform.position += transform.forward * Time.deltaTime * mSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            mScoreManager.UpdatePickUpCoins();
+            Destroy(other);
         }
     }
 
---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class ScoreManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text mText = null;
+
+    int count = 0;
+
+    void Start()
+    {
+        mText = Component.FindObjectOfType<Text>();
+    }
+
+    public void UpdatePickUpCoins()
+    {
+        count++;
+        mText.text = "Score: " + count;
+    }
+
+}
