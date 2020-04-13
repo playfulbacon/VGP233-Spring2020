08e71110882a5c89203b6cd4f2be55286b9adf92
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2020-04-13 14:08:03 -0700

    Update week1
    - Create Collectable script just to make the transform rotate from own axis.

---
+++ Assets/Script/Collectable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private readonly float mSpeed = 100;
+
+    void Update()
+    {
+        transform.Rotate(0.0f, mSpeed * Time.deltaTime, 0.0f);
+    }
+}
