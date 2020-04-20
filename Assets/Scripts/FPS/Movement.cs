18761c5a3e5274c57273ec201141500e56603def
Andy Bacon <bacon.andrew@gmail.com>
2020-04-20 14:52:50 -0700

    Movement using character controller.

--- Assets/Scripts/FPS/Movement.cs
+++ Assets/Scripts/FPS/Movement.cs
@@ -4,35 +4,20 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public GameObject player;
+    [SerializeField]
+    CharacterController controller;
+
     [SerializeField]
     private float movementSpeed = 15.0f;
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.LeftShift))
-        {
             movementSpeed *= 3;
-        }
         else if(Input.GetKeyUp(KeyCode.LeftShift))
-        {
             movementSpeed /= 3;
-        }
-        if(Input.GetKey(KeyCode.W))
-        {
-            player.transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
-        }
-        if(Input.GetKey(KeyCode.S))
-        {
-            player.transform.Translate(-Vector3.forward * movementSpeed * Time.deltaTime);
-        }
-        if(Input.GetKey(KeyCode.A))
-        {
-            player.transform.Translate(Vector3.right * -movementSpeed * Time.deltaTime);
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            player.transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
 
-        }
+        Vector3 move = (controller.transform.right * Input.GetAxis("Horizontal")) + (controller.transform.forward * Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * movementSpeed);
     }
 }
