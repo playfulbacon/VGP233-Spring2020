ad01cfea0396305e04068550df2f60240aaf0956
Andy Bacon <bacon.andrew@gmail.com>
2020-04-20 14:17:22 -0700

    TMP import, score UI.

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    public event System.Action<int> OnScoreChanged;
+
+    private int score = 0;
+
+    void Start()
+    {
+        FindObjectOfType<Shooter>().OnShoot += () => AddScore(1);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void AddScore(int add)
+    {
+        score += add;
+        OnScoreChanged?.Invoke(score);
+    }
+}
---
+++ Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUI : MonoBehaviour
+{
+    [SerializeField]
+    Text scoreText;
+
+    private void Awake()
+    {
+        FindObjectOfType<ScoreController>().OnScoreChanged += (x) => { scoreText.text = "Score: " + x.ToString(); };
+    }
+
+    void Update()
+    {
+        
+    }
+}
