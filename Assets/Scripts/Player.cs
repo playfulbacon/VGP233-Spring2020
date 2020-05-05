663cab29668de0d3d58bedb3a4f2d4b1a60cee25
Leon Freire <leonvfreire@gmail.com>
2020-05-04 17:43:47 -0700

    Week 5 - Class finished

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private CharacterController characterController;
+    private Vector3 moveDirection = Vector3.zero;
+    private Camera cam;
+
     [SerializeField]
-    float speed = 1f;
+    float moveSpeed = 7f;
 
-    void Start()
+    [SerializeField]
+    float jumpHeight = 10f;
+
+    [SerializeField]
+    float gravity = 9.8f;
+
+    private void Awake()
     {
-        List<LaneController.Lane> lanes = FindObjectOfType<LaneController>().LevelSegments[0].lanes;
-        transform.position = lanes[lanes.Count / 2].laneSegments[0].transform.position;
+        characterController = GetComponent<CharacterController>();
+        cam = FindObjectOfType<Camera>();
     }
-
-    void Update()
+    
+    private void Update()
     {
-        transform.position += transform.forward * Time.deltaTime * speed;
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            moveSpeed *= 2f;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            moveSpeed /= 2f;
+        }
+
+        if (characterController.isGrounded)
+        {
+            Vector3 forward = cam.transform.forward.normalized;
+            forward.y = 0f;
+            Vector3 right = cam.transform.right.normalized;
+            right.y = 0f;
+
+            moveDirection = (forward * Input.GetAxis("Vertical")) + (right * Input.GetAxis("Horizontal"));
+            moveDirection *= moveSpeed;
+
+            if (Input.GetButtonDown("Jump"))
+                moveDirection.y = jumpHeight;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    private Transform player;
+
+    [SerializeField]
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>().transform;
+    }
+    
+    private void LateUpdate()
+    {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X"), Vector3.up) * offset;
+        transform.position = player.transform.position + offset;
+        transform.LookAt(player.position);
+    }
+}
