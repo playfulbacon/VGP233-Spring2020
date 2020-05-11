00cbea69dca677070608d6a9a87bc4d609073c50
wallacegao <13730852881@163.com>
2020-05-11 16:48:01 +0900

    finish Character run and jump

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    Camera cam;
+
+    [SerializeField]
+    float moveSpeed = 7f;
+
+    [SerializeField]
+    float jumpHeight = 4f;
+
+    private CharacterController characterController;
+    private Vector3 moveDirection;
+
+    [SerializeField]
+    float gravity = 20f;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            //moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+            Vector3 forward = cam.transform.forward.normalized;
+            forward.y = 0f;
+            Vector3 right = cam.transform.right.normalized;
+            right.y = 0f;
+            
+            moveDirection = (forward * Input.GetAxis("Vertical")) + (right * Input.GetAxis("Horizontal"));
+
+            moveDirection *= moveSpeed;
+
+            if (Input.GetButtonDown("Jump"))
+                moveDirection.y = jumpHeight;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField]
+    Transform model;
+
+    private CharacterController characterController;
+    private Animator animator;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        model.transform.forward = characterController.velocity.normalized;
+
+        animator.SetFloat("MoveSpeed", characterController.velocity.magnitude);
+    }
+}
