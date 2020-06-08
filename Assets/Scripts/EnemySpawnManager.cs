e7c7cc3d2672ecc750de72243d312bc520b6a144
Stan <stanpal21@gmail.com>
2020-06-08 01:44:04 -0700

    Added an Enemy Spawn cap

---
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private Queue<GameObject> enemyList = new Queue<GameObject>();
+    [SerializeField]
+    private int maxSpawn = 5;
+    public Queue<GameObject> Enemies { get { return enemyList; } }
+
+    public bool canSpawn()
+    {
+        if (Enemies.Count <= maxSpawn)
+            return true;
+        return false;
+    }
+
+
+}
--- Assets/Scripts/TwitchCommands.cs
+++ Assets/Scripts/TwitchCommands.cs
@@ -25,12 +25,14 @@ public class TwitchCommands : MonoBehaviour
     private PlayerStats playerStats;
     private PlayerController playerController;
     private PlatFormManager platform;
+    private EnemySpawnManager SpawnManager;
 
     private void Awake()
     {
         platform = FindObjectOfType<PlatFormManager>();
         playerStats = FindObjectOfType<PlayerStats>();
         playerController = FindObjectOfType<PlayerController>();
+        SpawnManager = FindObjectOfType<EnemySpawnManager>();
         TwitchChat twitchChat = FindObjectOfType<TwitchChat>();
         twitchChat.OnMessageReceived += (x) => twitchCommands(x);
         
@@ -65,11 +67,19 @@ public class TwitchCommands : MonoBehaviour
 
         if (twitchMessage.ToLower() == twitchMessageToSpawn.ToLower())
         {
-            int xPosIndex = Random.Range(0,1);
+            int xPosIndex = Random.Range(0,2);
             Vector3 spawn = new Vector3
                 (playerController.transform.position.x + xPositions[xPosIndex],
                 playerController.transform.position.y);
-            Instantiate(enemyPrefab, spawn , Quaternion.identity);
+            if (SpawnManager.canSpawn())
+            {
+                SpawnManager.Enemies.Enqueue(Instantiate(enemyPrefab, spawn, Quaternion.identity));
+            }
+            else
+            {
+                Destroy(SpawnManager.Enemies.Dequeue());
+                SpawnManager.Enemies.Enqueue(Instantiate(enemyPrefab, spawn, Quaternion.identity));
+            }
         }
 
         if(twitchMessage.ToLower() == twitchMessageMoveUp.ToLower())
